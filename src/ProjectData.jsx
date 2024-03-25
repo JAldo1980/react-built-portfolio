9d3784f7c24c53644938b3259a77d86c4f0fa558
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2024-03-25 10:19:38 +0000

    Update ProjectData.jsx

--- src/ProjectData.jsx
+++ src/ProjectData.jsx
@@ -1,8 +1,19 @@
 export default [
+
+  {
+    headline: "Monzo Bank Clone (Home Page Only)",
+    description:
+      "Focused on replicating UI and proffesional website presentation",
+    tech: ["React", "JavaScript", "Tailwind"],
+    buttonOne: "https://github.com/JAldo1980/monzo-bank-clone",
+    buttonTwo: "https://monzo-clone.netlify.app/",
+    image: "public/monzo-img-removebg-preview.png",
+  },
+
   {
     headline: "Small Business Landing Page",
     description:
-      "Glow is a small business with a focus on deliving LinkedIn personal branding services and professional personal websites for the individual.",
+      "Glow is a small business with a focus on delivering LinkedIn personal branding services and professional personal websites for the individual.",
     tech: ["React", "Node", "JavaScript"],
     buttonOne: "https://github.com/JAldo1980/small-business-landing-page",
     buttonTwo: "https://jaldo1980.github.io/small-business-landing-page/",
